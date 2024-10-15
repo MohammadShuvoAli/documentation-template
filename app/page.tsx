d03c9d88b32c90665c235b142f21fc762200541a
shuvomascot <mohammadshuvoali@gmail.com>
2024-10-15 21:46:02 +1100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -11,14 +11,16 @@ export default function Home() {
         target="_blank"
         className="mb-5 sm:text-lg flex items-center gap-2 underline underline-offset-4"
       >
-        Follow on GitHub{" "}
+        Follow along on GitHub{" "}
         <MoveUpRightIcon className="w-4 h-4 font-extrabold" />
       </Link>
       <h1 className="text-3xl font-bold mb-4 sm:text-7xl">
-        Exsited SDK
+        An example app built using Next.js and server components.
       </h1>
       <p className="mb-8 sm:text-xl max-w-[800px] text-muted-foreground">
-        Leading payment processing platform. That's simple to use and easy to customize.
+        This feature-packed documentation template, built with Next.js, offers a
+        sleek and responsive design, perfect for all your project documentation
+        needs.
       </p>
       <div className="flex flex-row items-center gap-5">
         <Link
