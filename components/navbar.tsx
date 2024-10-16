309878d877e160167d2485b8fb8c58adc9dfea58
shuvomascot <mohammadshuvoali@gmail.com>
2024-10-16 14:18:39 +1100

    update

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -78,7 +78,7 @@ export function Navbar() {
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2.5">
-      <CommandIcon className="w-6 h-6 text-muted-foreground" strokeWidth={2} />
+      {/* <CommandIcon className="w-6 h-6 text-muted-foreground" strokeWidth={2} /> */}
       <h2 className="text-md font-bold font-code">Exsited SDK</h2>
     </Link>
   );
